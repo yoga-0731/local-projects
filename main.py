7a7f1d073fe4f694b28fd4e3568169ccd9721941
Yogarajalakshmi <17euec176@skcet.ac.in>
2023-05-12 11:45:01 +0530

    coffee maker using OOP

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello World!")
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+turn_off = False
+
+coffee_maker_obj = CoffeeMaker()
+money = MoneyMachine()
+menu = Menu()
+
+while not turn_off:
+    print("Welcome 😁 !!!")
+
+    prompt = input(f"What would you like? ({menu.get_items()}): ").lower()
+    if prompt == 'report':
+        coffee_maker_obj.report()
+        money.report()
+    elif prompt == 'off':
+        turn_off = True
+    else:
+        drink = menu.find_drink(prompt)
+        if coffee_maker_obj.is_resource_sufficient(drink) and money.make_payment(drink.cost):
+            coffee_maker_obj.make_coffee(drink)
